6c15c65e4ccacdf29bedecbc329a9c0d245b9106
Teemu Kokkonen <teemu@fastmonkeys.com>
2014-06-27 12:46:19 +0300

    Operations

--- stellar/database.py
+++ stellar/database.py
@@ -7,6 +7,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 stellar_db = create_engine(config['stellar_url'], echo=False)
 db = create_engine(config['url'], echo=False)
+raw_connection = db.connect()
+raw_connection.connection.set_isolation_level(0)
 Base = declarative_base()
 db.session = sessionmaker(bind=db)()
 stellar_db.session = sessionmaker(bind=stellar_db)()
--- stellar/operations.py
+++ stellar/operations.py
@@ -1,16 +1,45 @@
 from sqlalchemy.exc import ProgrammingError
-from database import stellar_db, db, Base
+from database import stellar_db, db, Base, raw_connection
 from models import Snapshot
 
 def create_stellar_tables():
-    connection = db.connect()
-    connection.connection.set_isolation_level(0)
     try:
-        connection.execute('''
+        raw_connection.execute('''
             CREATE DATABASE "stellar_data"
         ''')
     except ProgrammingError:
         return False
-    connection.close()
     create_stellar_tables()
     return True
+
+
+def copy_database(from_database, to_database):
+    raw_connection.execute(
+        '''
+            CREATE DATABASE "%s" WITH TEMPLATE "%s";
+        ''' % (
+            to_database,
+            from_database
+        )
+    )
+
+
+def rename_database(from_database, to_database):
+    raw_connection.execute(
+        '''
+            ALTER DATABASE "%s" RENAME TO "%s"
+        ''' % (
+            from_database,
+            to_database
+        )
+    )
+
+
+def rename_database(database):
+    raw_connection.execute(
+        '''
+            DROP DATABASE "%s"
+        ''' % (
+            database
+        )
+    )

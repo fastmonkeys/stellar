fb7de2a8771754a57fc0569e55da91093e1bcc5f
Pekka Pöyry <pekka.poyry@fastmonkeys.com>
2014-06-27 13:36:55 +0300

    Reset database connection before copy

--- stellar/operations.py
+++ stellar/operations.py
@@ -1,6 +1,6 @@
 from sqlalchemy.exc import ProgrammingError
-from database import stellar_db, db, Base, raw_connection
-from models import Snapshot
+from database import stellar_db, Base, raw_connection
+
 
 def create_stellar_tables():
     try:
@@ -17,11 +17,25 @@ def create_stellar_tables():
     return True
 
 
+def terminate_database_connections(database):
+    raw_connection.execute(
+        '''
+            SELECT pg_terminate_backend(pg_stat_activity.pid)
+            FROM pg_stat_activity
+            WHERE
+                pg_stat_activity.datname = '%s' AND
+                pid <> pg_backend_pid();
+        ''' % database
+    )
+
+
 def copy_database(from_database, to_database):
+    terminate_database_connections(from_database)
     raw_connection.execute(
         '''
             CREATE DATABASE "%s" WITH TEMPLATE "%s";
-        ''' % (
+        ''' %
+        (
             to_database,
             from_database
         )
@@ -32,7 +46,8 @@ def rename_database(from_database, to_database):
     raw_connection.execute(
         '''
             ALTER DATABASE "%s" RENAME TO "%s"
-        ''' % (
+        ''' %
+        (
             from_database,
             to_database
         )

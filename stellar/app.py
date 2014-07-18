57574d59f18051a13f17795f96f189dcb19a2668
Pekka Pöyry <pekka.poyry@fastmonkeys.com>
2014-07-18 13:04:29 +0300

    Make listing snapshots work

--- stellar/app.py
+++ stellar/app.py
@@ -4,6 +4,7 @@ import uuid
 import os
 
 from config import config
+from models import *
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import MultipleResultsFound
@@ -14,15 +15,19 @@ from sqlalchemy.ext.declarative import declarative_base
 class Stellar(object):
     def __init__(self):
         self.init_database()
-        self.create_stellar_database()
 
     def init_database(self):
-        self.db = create_engine(config['stellar_url'], echo=False)
         self.raw_db = create_engine(config['url'], echo=False)
-        self.raw_conn = self.db.connect()
+        self.raw_conn = self.raw_db.connect()
         self.raw_conn.connection.set_isolation_level(0)
-        self.db.session = sessionmaker(bind=self.db)()
         self.raw_db.session = sessionmaker(bind=self.raw_db)()
+        tables_missing = self.create_stellar_database()
+
+        self.db = create_engine(config['stellar_url'], echo=False)
+        self.db.session = sessionmaker(bind=self.db)()
+
+        if tables_missing:
+            self.create_stellar_tables()
 
         logging.basicConfig()
         logging.getLogger('sqlalchemy.engine').setLevel(logging.WARN)
@@ -32,28 +37,30 @@ class Stellar(object):
             self.raw_conn.execute('''
                 CREATE DATABASE "stellar_data"
             ''')
+            return True
         except ProgrammingError:
             return False
-        self.base.metadata.create_all(self.db)
+
+    def create_stellar_tables(self):
+        Base.metadata.create_all(self.db)
         self.db.session.commit()
 
     def get_snapshot(self, snapshot_name):
-        return self.stellar_db.session.query(Snapshot).filter(
+        return self.db.session.query(Snapshot).filter(
             Snapshot.name == snapshot_name,
         ).first()
 
     def get_snapshots(self):
-        return self.stellar_db.session.query(
+        return self.db.session.query(
             Snapshot
         ).order_by(
             Snapshot.created_at.desc()
         ).all()
 
     def get_latest_snapshot(self):
-        return self.stellar_db.session.query(Snapshot).filter(
+        return self.db.session.query(Snapshot).filter(
             Snapshot.project_name == config['project_name']
         ).order_by(Snapshot.created_at.desc()).first()
-
 
     def create_snapshot(self, snapshot_name):
         for table_name in config['tracked_databases']:
@@ -65,19 +72,19 @@ class Stellar(object):
                 project_name=config['project_name'],
                 name=snapshot_name,
             )
-            stellar_db.session.add(snapshot)
-        stellar_db.session.commit()
+            db.session.add(snapshot)
+        db.session.commit()
         if os.fork():
             return
 
         for table_name in config['tracked_databases']:
-            snapshot = stellar_db.session.query(Snapshot).filter(
+            snapshot = db.session.query(Snapshot).filter(
                 Snapshot.table_name == table_name,
                 Snapshot.name == snapshot_name,
             ).one()
             copy_database(table_name, 'stellar_%s_slave' % snapshot.table_hash)
             snapshot.is_slave_ready = True
-            stellar_db.session.commit()
+            db.session.commit()
 
     def remove_snapshot(self, snapshot):
         for table_hash in (s.table_hash for s in snapshots):
@@ -90,10 +97,10 @@ class Stellar(object):
             except ProgrammingError:
                 pass
         snapshots.delete()
-        stellar_db.session.commit()
+        db.session.commit()
 
     def restore(self, snapshot):
-        for snapshot in stellar_db.session.query(Snapshot).filter(
+        for snapshot in db.session.query(Snapshot).filter(
             Snapshot.name == name,
             Snapshot.project_name == config['project_name']
         ):
@@ -110,12 +117,12 @@ class Stellar(object):
                 snapshot.table_name
             )
             snapshot.is_slave_ready = False
-            stellar_db.session.commit()
+            db.session.commit()
 
         if os.fork():
             return
 
-        for snapshot in stellar_db.session.query(Snapshot).filter(
+        for snapshot in db.session.query(Snapshot).filter(
             Snapshot.name == name,
             Snapshot.project_name == config['project_name']
         ):
@@ -124,27 +131,26 @@ class Stellar(object):
                 'stellar_%s_slave' % snapshot.table_hash
             )
             snapshot.is_slave_ready = True
-            stellar_db.session.commit()
+            db.session.commit()
 
         sys.exit()
 
     def get_orphan_snapshots(self):
-        from database import stellar_db, db
         from models import Snapshot
         from operations import remove_database
 
         databases = set()
         stellar_databases = set()
-        for snapshot in stellar_db.session.query(Snapshot):
+        for snapshot in db.session.query(Snapshot):
             stellar_databases.add(snapshot.table_name)
 
-        for row in db.execute('''
+        for row in self.raw_db.execute('''
             SELECT datname FROM pg_database
             WHERE datistemplate = false
         '''):
             databases.add(row[0])
 
         return filter(
-            lambda database: if database.startswith('stellar_') and database != 'stellar_data',
+            lambda database: database.startswith('stellar_') and database != 'stellar_data',
             (databases-stellar_databases)
         )

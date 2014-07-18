acca791c30c6cbe201554f4eddedbd0236aabd65
Teemu Kokkonen <teemu@fastmonkeys.com>
2014-07-18 12:11:37 +0300

    Fixed models

--- stellar/models.py
+++ stellar/models.py
@@ -6,12 +6,32 @@ from database import Base
 class Snapshot(Base):
     __tablename__ = 'snapshot'
     id = sa.Column(sa.Integer, sa.Sequence('snapshot_id_seq'), primary_key=True)
-    table_name = sa.Column(sa.String(255), nullable=False)
-    table_hash = sa.Column(sa.String(32), nullable=False)
+    snapshot_name = sa.Column(sa.String(255), nullable=False)
     project_name = sa.Column(sa.String(255), nullable=False)
-    name = sa.Column(sa.String(255), nullable=False)
+    hash = sa.Column(sa.String(32), nullable=False)
     created_at = sa.Column(sa.DateTime, default=datetime.utcnow)
-    is_slave_ready = sa.Column(sa.Boolean, default=False)
+
+    def __repr__(self):
+        return "Snapshot(name=%r, name=%r)" % (
+            self.table_name,
+            self.name
+        )
+
+
+class Table(Base):
+    __tablename__ = 'snapshot'
+    id = sa.Column(sa.Integer, sa.Sequence('table_id_seq'), primary_key=True)
+    table_name = sa.Column(sa.String(255), nullable=False)
+    snapshot_id = sa.Column(sa.Integer, db.ForeignKey(Snapshot.id), nullable=False)
+    snapshot = sa.relationship(Snapshot, backref='tables')
+    slave_pid = sa.Column(sa.Integer, nullable=True)
+
+    def table_name(self, postfix):
+        return 'stellar_%s_%s_%s' % (
+            self.name,
+            self.snapshot.hash,
+            postfix
+        )
 
     def __repr__(self):
         return "Snapshot(table_name=%r, name=%r)" % (
